e60f3594ba104ef40e89a9669cafa79fe4462324
Coded.om <code.dmt@gmail.com>
2025-07-24 09:54:15 +0400

    Solve parrot

---
+++ Warmup-1/ParrotTrouble.java
@@ -0,0 +1,15 @@
+
+public class ParrotTrouble {
+    public static void main(String[] arges) {
+        
+    }
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if ((hour > 20 || hour < 7) && talking) {
+            return true;
+        }
+
+        return false;
+
+}
+    
+}
