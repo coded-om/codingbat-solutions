061009e673d319f96ace2a276c2e9206777bac5b
Coded.om <code.dmt@gmail.com>
2025-07-24 10:10:43 +0400

    Solve nearHundred

---
+++ Warmup-1/nearHundred.java
@@ -0,0 +1,15 @@
+
+public class nearHundred {
+    public static void main(String[] args) {
+    }
+
+    public boolean nearHundred(int n) {
+        if ((n >= 90 && n <= 110) || (n >= 190 && n <= 210)) {
+            return true;
+        } 
+        else {
+            return false;
+            }
+}
+    
+}
