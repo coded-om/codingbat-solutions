259ec4e8b29ef37bf41fe34b1b7c2c53fa951549
Coded.om <code.dmt@gmail.com>
2025-07-24 09:35:56 +0400

    Solve

---
+++ Warmup-1/SumDouble.java
@@ -0,0 +1,14 @@
+
+public class SumDouble {
+    public static void main(String[] args) {
+        
+    }
+    
+    public int sumDouble(int a, int b) {
+        if (a == b) {
+            return (a + b) * 2;
+        } else {
+            return a + b;
+        }
+    }
+}
