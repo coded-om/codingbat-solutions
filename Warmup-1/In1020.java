0f19af131f6a043f60564d3ae74a9cb8c1ccfb4c
Coded.om <code.dmt@gmail.com>
2025-07-27 02:50:32 +0400

    remove unnecessary blank line in In1020 class

--- Warmup-1/In1020.java
+++ Warmup-1/In1020.java
@@ -3,7 +3,6 @@ public class In1020 {
         System.out.println(in1020(21, 12));  
         System.out.println(in1020(8, 99));   
     }
-
     public static boolean in1020(int a, int b) {
         boolean aInRange = (a >= 10 && a <= 20);
         boolean bInRange = (b >= 10 && b <= 20);
