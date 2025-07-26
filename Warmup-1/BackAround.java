00f71068b1d0c15d6288f6126db28c5f92faf2a3
Coded.om <code.dmt@gmail.com>
2025-07-27 02:14:02 +0400

    add BackAround class to implement string manipulation with surrounding characters

---
+++ Warmup-1/BackAround.java
@@ -0,0 +1,13 @@
+public class BackAround {
+        public static void main(String[] args) {
+        System.out.println(backAround("cat"));  
+        System.out.println(backAround("a"));     
+        System.out.println(backAround("hello")); 
+    }
+
+    public static String backAround(String str) {
+        String last = str.substring(str.length() - 1);
+        return last + str + last;
+    }
+    
+}
