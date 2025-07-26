4a781b204aba096a44c5512dcb9b2828cf8ebe42
Coded.om <code.dmt@gmail.com>
2025-07-27 02:14:55 +0400

    Solve it with less lines of code.

--- Warmup-1/BackAround.java
+++ Warmup-1/BackAround.java
@@ -1,8 +1,6 @@
 public class BackAround {
         public static void main(String[] args) {
         System.out.println(backAround("cat"));  
-        System.out.println(backAround("a"));     
-        System.out.println(backAround("hello")); 
     }
 
     public static String backAround(String str) {
