8aa3396402786529860acad673a809cc2d05d799
Coded.om <code.dmt@gmail.com>
2025-07-29 01:32:57 +0400

    refactor StringExample class to update method names and improve greeting functionality

--- String-1/StringExample.java
+++ String-1/StringExample.java
@@ -2,12 +2,12 @@ public class StringExample {
     
     public static void main(String[] args) {
         StringExample se = new StringExample();
-        System.out.println(se.example("Mohammed"));
-        System.out.println(se.example("Ali"));
-        System.out.println(se.example(""));
+        System.out.println(se.helloName("Mohammed"));
+        System.out.println(se.helloName("Ali"));
+        System.out.println(se.helloName(""));
     }
     
-    public String example(String name) {
-        return "xyz" + name;
+    public String helloName(String name) {
+        return "Hello " + name + "!";
     }
 }
