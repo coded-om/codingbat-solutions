57092f2a09e28c947b93ce9479a27a1657e97bc8
Coded.om <code.dmt@gmail.com>
2025-07-29 01:06:15 +0400

    update

--- String-1/StringExample.java
+++ String-1/StringExample.java
@@ -2,9 +2,8 @@ public class StringExample {
     
     public static void main(String[] args) {
         StringExample se = new StringExample();
-        System.out.println(se.example("Bob"));
-        System.out.println(se.example("Alice"));
-        System.out.println(se.example("John"));
+        System.out.println(se.example("Mohammed"));
+        System.out.println(se.example("Ali"));
         System.out.println(se.example(""));
     }
     
